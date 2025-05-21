50dedc5102b40800d5060f1af1987be2d9ce0de5
NathnelF <136366777+NathnelF@users.noreply.github.com>
2025-05-21 18:39:11 -0400

    Added initial struturec + TODOs for contract table

---
+++ src/routes/contracts/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import ContractTable from "./ContractTable.svelte";
+    const contracts = [
+        {
+            "id": 1,
+            "customerId": 1,
+            "serviceId": 1,
+            "price": 5000,
+            "currentTermStart": "2025-05-20T17:32:14.086",
+            "currentTermEnd": "2026-05-19T17:32:14.086",
+            "termLength": 12,
+            "originalContractStart": "2025-05-20T17:32:14.086",
+            "invoiceDate": null,
+            "isAutoRenew": true,
+            "isChurned": false
+        },
+        {
+            "id": 2,
+            "customerId": 1,
+            "serviceId": 1,
+            "price": 1000,
+            "currentTermStart": "2024-05-20T17:31:16.333",
+            "currentTermEnd": "2025-05-19T17:31:16.333",
+            "termLength": 12,
+            "originalContractStart": "2024-05-20T17:31:16.333",
+            "invoiceDate": null,
+            "isAutoRenew": true,
+            "isChurned": null
+        }
+    ];
+
+    const names = {
+        customerName: "Test Customer",
+        serviceName: "Test Service"
+    };
+
+</script>
+
+<ContractTable contracts={contracts} names={names} />
---
+++ src/routes/contracts/ContractTable.svelte
@@ -0,0 +1,40 @@
+<script>
+    let {contracts, names} = $props();
+
+    //TODO: await contracts and render them if they exist
+
+    //If they don't direct to new contract page.
+
+    //TODO: add edit contract page
+</script>
+
+<div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+  <table class="table text-left">
+    <thead>
+      <tr>
+        <th>Contract Id</th>
+        <th>Customer</th>
+        <th>Service</th>
+        <th>Annual Amount</th>
+        <th>Current Term Start</th>
+        <th>Current Term End</th>
+        <th class="text-center">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each contracts as contract, i}
+      <tr>
+        <th>{contract.id}</th>
+        <td>{names.customerName}</td>
+        <td>{names.serviceName}</td>
+        <td>{contract.price}</td>
+        <td>{contract.currentTermStart}</td>
+        <td>{contract.currentTermEnd}</td>
+        <td class="text-center">
+            <a class="btn btn-ghost text-m" href="contracts/{contract.id}/schedule">Schedule</a>
+        </td>
+      </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
