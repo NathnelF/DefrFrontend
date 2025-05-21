f8d11ab1675bf656bc41f1c7f733a2cd1f97a512
NathnelF <136366777+NathnelF@users.noreply.github.com>
2025-05-21 18:43:42 -0400

    Added initial structure + TODOS for report page

---
+++ src/routes/report/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+    import Report from './Report.svelte'
+    //TODO: default to events from the current month / year
+    //TODO: add date picker to choose the time period for the reports generated 
+    const events = [
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-05-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-06-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-07-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-08-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-09-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-10-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-11-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2025-12-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2026-01-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2026-02-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2026-03-15T19:57:58.387"
+      },
+      {
+        "contractId": 1,
+        "amount": 0,
+        "date": "2026-04-15T19:57:58.387"
+      }
+    ];
+</script>
+
+<Report events={events} />
---
+++ src/routes/report/Report.svelte
@@ -0,0 +1,24 @@
+<script> 
+    let { events } = $props();
+</script>
+
+<div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+  <table class="table text-left">
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Contract</th>
+        <th>Balance</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each events as event}
+      <tr>
+        <td>{event.date}</td>
+        <td>{event.contractId}</td>
+        <td>{event.amount}</td>
+      </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
