393262bad960368a17fc76c18254aa8486a8f29d
NathnelF <136366777+NathnelF@users.noreply.github.com>
2025-05-21 18:31:58 -0400

    added navbar plus rotating cat image as temporary home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,12 @@
 	let { children } = $props();
 </script>
 
+<div class="navbar bg-base-100 shadow-sm">
+	<a class="btn btn-ghost text-xl" href="/">Home</a>
+	<a class="btn btn-ghost text-xl" href="/contracts">Contracts</a>
+	<a class="btn btn-ghost text-xl" href="/report">Report</a>
+</div>
+
+
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,27 @@
-<button class='btn'>Test</button>
+<script>
+    let xflip = -1;
+    let yflip = -1;
+
+    setInterval(() => {
+        xflip *= -1;
+    }, 1000);
+
+    setInterval(() => {
+        yflip *= -1;
+    }, 1320);
+
+</script>
+
+<main class="flex items-center justify-center min-h-screen">
+    <div class="avatar">
+        <div class="w-65 rounded-xl">
+            <img src="https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp" alt="woman in tears pointing at cat"/>
+        </div>
+    </div>
+    <div class="avatar">
+        <div class="w-65 rounded-full"
+        style="transform: scaleX({xflip}) scaleY({yflip});">
+            <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" alt="smug cat laughing at crying woman"/>
+        </div>
+    </div>
+</main>
