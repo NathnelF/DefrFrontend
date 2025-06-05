1cfccd17c684c60013b3e681e8530afce068ac67
NathnelF <136366777+NathnelF@users.noreply.github.com>
2025-06-05 10:19:56 -0400

    Added modal with initial counts on compare page.

--- src/routes/compare/+page.svelte
+++ src/routes/compare/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import CompareReport from "./CompareReport.svelte"
     import Flatpickr from "$lib/components/Flatpickr.svelte";
+    import { disableScrollHandling } from "$app/navigation";
     let currentDay = new Date();
     let firstDayOfYear = new Date(currentDay.getFullYear(), 0, 1);
 
@@ -11,6 +12,7 @@
     let nwgError = $state(null)
     let qbError = $state(null)
     let found = $state(false)
+    let countModal = $state(null)
 
     async function fetchEvents(){
         const qbUrl = `https://localhost:7246/get_qb_event_range?startDate=${startDate}&endDate=${endDate}`
@@ -54,6 +56,14 @@
         nwgError = null
         found = false
     }
+
+    function open(){
+        countModal.showModal()
+    }
+
+    function close(){
+        countModal.close()
+    }
 </script>
 
 <div class="flex items-center mb-4">
@@ -61,6 +71,59 @@
   <Flatpickr id={"end-date"} label="To" bind:value={endDate}/>
   <button class="btn mt-6.5 ml-3 px-10 py-5 rounded-md" onclick={fetchEvents}>Get Events</button>
   <button class="btn mt-6.5 ml-3 px-10 py-5 rounded-md" onclick={clearEvents}>Clear Events</button>
+  <button class="btn mt-6.5 ml-3 px-10 py-5 rounded-md" onclick={ open }>See counts</button>
+  <dialog class="modal" bind:this={countModal}>
+    <div class="modal-box w-11/12 max-w-5xl">
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <p class="py-4">Click the button below to close</p>
+        <div class="flex w-full">
+            {#if nwgEvents.length > 0}
+            <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Customer</th>
+                        <th>Count</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {#each nwgEvents as nwgEvent}
+                    <tr>
+                        <th>{nwgEvent.customerInfo}</th>
+                        <td>{nwgEvent.amount}</td>
+                    </tr>
+                    {/each}
+                    </tbody>
+                </table>
+                </div>
+                {/if}
+            <div class="divider divider-horizontal"></div>
+            {#if qbEvents.length > 0}
+            <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Customer</th>
+                        <th>Count</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {#each qbEvents as qbEvent}
+                    <tr>
+                        <th>{qbEvent.customerInfo}</th>
+                        <td>{qbEvent.amount}</td>
+                    </tr>
+                    {/each}
+                    </tbody>
+                </table>
+                </div>
+                {/if}
+        </div>
+        <div class="modal-action">
+            <button class="btn" onclick={ close }>Close</button>
+        </div>
+    </div>
+  </dialog>
 </div>
 
 
